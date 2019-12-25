3193bbe2905d7adb366b2ee6296ff3c00229c190
Yashvendra <yashkashyap00720@gmail.com>
2019-12-25 15:59:14 +0530

    Updating install.sh

--- install.sh
+++ install.sh
@@ -1,49 +1,51 @@
 #!/bin/bash
-
+Green='\033[0;32m'
 white='\033[0;37m'
 NC='\033[0m'
 clear
 printf '\033]2; INSTALLER\a'
-echo -e "Press \e[1;33many key\e[0m to install the script..."
+echo -e "${Green}[*] Press \e[0;33many key\e[0;32m to install AutoXploit..."
 read -n 1 
 clear
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-if [[ "$DIR" != "/root/autoxploit" ]]
+if [[ "$DIR" != "/root/AutoXploit" ]]
 then
-	echo -e "I will try to install it for you..."
+	echo -e "\033[0;35m[~] I will install it for you..."
 	sleep 4
-	if [[ -d /root/autoxploit ]]
+	if [[ -d /root/AutoXploit ]]
 	then 
-		rm -r /root/autoxploit
+		rm -r /root/AutoXploit
 	fi
-	mkdir /root/autoxploit
-	cp -r "$DIR"/* /root/autoxploit
-	chmod +x /root/autoxploit/install.sh
-	#gnome-terminal -- bash -c "sudo /root/autoxploit/install.sh; exec bash"
+	mkdir /root/AutoXploit
+	cp -r "$DIR"/* /root/AutoXploit
+	chmod +x /root/AutoXploit/install.sh
+	#gnome-terminal -- bash -c "sudo /root/bootmiester/install.sh; exec bash"
 fi
-echo -e "Installing AutoXploit..."
+echo -e "${Green}[+] Installing AutoXploit..."
 sleep 1
-echo -e "Fixing permissions..."
+echo -e "${Green}[+] Fixing permissions..."
 sleep 2
-chmod +x /root/autoxploit/autoxploit.sh
+chmod +x /root/AutoXploit/autoxploit.py
 clear
-echo -e "Copying script to /bin/autoxploit"
-cd /root/autoxploit
-cp /root/autoxploit/autoxploit.sh /bin/autoxploit
+echo -e "${Green}[+] Copying Tool to /bin/autoxploit"
+cd /root/AutoXploit
+cp /root/AutoXploit/autoxploit.py /bin/autoxploit
 clear
+
 while true
 do  
 	clear
-	echo -e "Are you \e[1;33mu\e[0mpdating or \e[1;33mi\e[0mnstalling the script?(\e[1;33mu\e[0m/\e[1;33mi\e[0m): "
-	echo -e "Only use 'i' for the first time."
+	echo -e "${Green}[*] Are you \e[0;33mu\e[0;32mpdating or \e[0;33mi\e[0;32mnstalling the script?(\e[0;33mu\e[0;32m/\e[0;33mi\e[0;32m): "
+	echo -e "${Green}[#] Only use 'i' for the first time."
 	read UORI
 	if [[ "$UORI" = "u" ]]
 	then 
 		clear 
 		echo -e "This feature is currently under construction.."
 		sleep 3
+		exit
 	elif [[ "$UORI" = "i" ]]
 	then 
 		clear
@@ -54,7 +56,7 @@ do
 			sleep 3
 			break
 		fi
-		echo -e "Adding AutoXploit to PATH so you can access it from anywhere"
+		echo -e "${Green}[#] Adding AutoXploit to PATH so you can access it from anywhere"
 		sleep 1
 		export PATH=/bin/autoxploit:$PATH
 		sleep 1
@@ -65,9 +67,9 @@ do
 	fi
 done
 sleep 1
-echo -e "Installation is finished. Type 'sudo autoxploit' to launch the script after we exit."
+echo -e "${Green}[#] Installation is finished. Type 'sudo autoxploit' to launch the script after we exit."
 sleep 0.5
-echo -en "Starting AutoXploit"; sleep 0.5 ;echo -en "." ;sleep 0.5 ;echo -en "." ;sleep 0.5 ;echo -en "." ;sleep 0.5 ;echo -en "." ;
+echo -en "${Green}[+] Starting AutoXploit"; sleep 0.5 ;echo -en "." ;sleep 0.5 ;echo -en "." ;sleep 0.5 ;echo -en "." ;sleep 0.5 ;echo -en "." ;
 sudo autoxploit
 
 
